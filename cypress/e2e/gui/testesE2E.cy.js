414e5233256478badd1dc80c0d240cb28a9a4c1a
Carlos Daniel <carlosdanieldossantos2011@hotmail.com>
2025-01-10 16:32:49 -0300

    Teste 021

--- cypress/e2e/gui/testesE2E.cy.js
+++ cypress/e2e/gui/testesE2E.cy.js
@@ -7,37 +7,37 @@ describe('Lista de Produto', () => {
    beforeEach(() => {
       cy.login(usuario, senha)
       cy.url().should('eq', "http://165.227.93.41/lojinha-web/v2/produto")
-      cy.screenshot()
+      
    });
    it.only('Criar um produto com sucesso', () => {
       cy.novoProduto('Celular iphone', '7000.00', 'Preto, Branco')
       cy.get('#btn-salvar').click()
       cy.url('http://165.227.93.41/lojinha-web/v2/produto')
-      cy.screenshot()
+      
    });
    it.only('Criar um Produto sem Nome', () =>{
       cy.novoProduto('','140.00','Violetas')
       cy.get('#btn-salvar').click()
       cy.url('http://165.227.93.41/lojinha-web/v2/produto')
-      cy.screenshot()
+      
 
    })
    it.only('Excluir Produto', () => {
       cy.get('.secondary-content').eq(0).click()
-      cy.screenshot()
+      
    })
    it.only('Criar um produto com valor maior que o permitido', () => {
       cy.produtoValorMaior('Garrafa de suco', '8000.00', 'Preto, Branco')
       cy.get('#btn-salvar').click()
       cy.url('http://165.227.93.41/lojinha-web/v2/produto?error')
-      cy.screenshot()
+      
       
    })
    it.only('Criar um Produto sem valor', () =>{
    cy.produtoValorMaior('Garrafa de suco','', 'Preto, Branco')
       cy.get('#btn-salvar').click()
       cy.url('http://165.227.93.41/lojinha-web/v2/produto?error')
-      cy.screenshot()
+     
    })
    it.only('Criar um Produto sem Cor', () =>{
       cy.produtoValorMaior('Garrafa de suco','150.00','')
@@ -51,30 +51,30 @@ describe('Lista de Produto', () => {
       cy.url('http://165.227.93.41/lojinha-web/v2/produto/editar')
       cy.get('.waves-effect').eq(1).click()
       cy.url().should('eq', 'http://165.227.93.41/lojinha-web/v2/produto')
-      cy.screenshot()
+      
    })
    it.only('Adicionar Componente com Sucesso', () => {
       cy.adicionarComponente('Fone de Ouvido', '10')
       cy.get('.waves-effect').eq(0).click()
       cy.url('http://165.227.93.41/lojinha-web/v2/produto/editar')
-      cy.screenshot()
+      
    })
    it.only('Excluir componente', () => {
       cy.editarProduto('Celular Samsung', '78.00', 'Azul, claro')
       cy.get('.secondary-content').eq(0).click()
       cy.url('http://165.227.93.41/lojinha-web/v2/produto/editar')
-      cy.screenshot()
+      
    })
 });
 describe('Login inválido', () => {
    it.only('Login com credencias inválidas', () => {
       cy.login(faker.person.firstName(), faker.internet.password())
       cy.url('contains', 'http://165.227.93.41/lojinha-web/v2/')
-      cy.screenshot()
+      
    })
    it.only('Login com credencias com caracteres especiais', () => {
       cy.login('@@@@@@','!!!!!!!')
       cy.url('contains', 'http://165.227.93.41/lojinha-web/v2/?error')
-      cy.screenshot()
+      
    })
 });
